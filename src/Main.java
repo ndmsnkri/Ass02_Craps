c43129a185a9e44132d311cc605b1a87a15e5531
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-10-18 09:32:28 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,69 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        boolean playAgain = true;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while (playAgain) {
+            int die1 = rnd.nextInt(6) + 1;
+            int die2 = rnd.nextInt(6) + 1;
+            int crapsRoll = die1 + die2;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println();
+            System.out.println("Rolling the dice...");
+            System.out.println();
+            System.out.println("Die 1: " + die1);
+            System.out.println("Die 2: " + die2);
+            System.out.println("Total: " + crapsRoll);
+
+            if (crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12) {
+                System.out.println();
+                System.out.println("Craps! You lose!");
+            } else if (crapsRoll == 7 || crapsRoll == 11) {
+                System.out.println();
+                System.out.println("Natural! You win!");
+            } else {
+                System.out.println();
+                System.out.println("Point is " + crapsRoll);
+                boolean pointMade = false;
+
+                while (true) {
+                    int nextDie1 = rnd.nextInt(6) + 1;
+                    int nextDie2 = rnd.nextInt(6) + 1;
+                    int nextRoll = nextDie1 + nextDie2;
+
+                    System.out.println();
+                    System.out.println("Rolling the dice again...");
+                    System.out.println();
+                    System.out.println("Die 1: " + nextDie1);
+                    System.out.println("Die 2: " + nextDie2);
+                    System.out.println("Total: " + nextRoll);
+
+                    if (nextRoll == crapsRoll) {
+                        System.out.println();
+                        System.out.println("Made the point! You win!");
+                        pointMade = true;
+                        break;
+                    } else if (nextRoll == 7) {
+                        System.out.println();
+                        System.out.println("Got a seven and lost!");
+                        break;
+                    }
+                }
+
+                if (!pointMade) {
+                    System.out.println();
+                    System.out.println("Point not made. You lose!");
+                }
+            }
+            System.out.println();
+            System.out.print("Play again? (y/n): ");
+            String playAgainInput = in.next().toLowerCase();
+            playAgain = playAgainInput.equals("y");
         }
     }
 }
